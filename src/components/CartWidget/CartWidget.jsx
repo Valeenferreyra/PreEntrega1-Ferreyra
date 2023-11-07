bd14eb6f51262ecf461035be4127c0a093652f8c
valeenferreyra <vfe.2001@gmail.com>
2023-11-06 21:14:16 -0300

    corregi un error del cartWidget

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ export const CartWidget = () => {
     return (
         <div className="d-flex">
             <FaCartShopping color="red" size={27}/>
-            <p className="mx-2">manuel</p>
+            <p className="mx-2">3</p>
         </div>
     )
 }
