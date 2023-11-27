62af4a910d8edf36f6844e4e1f2b2ac8cfc76c8b
valeenferreyra <vfe.2001@gmail.com>
2023-11-26 21:37:12 -0300

    correccion de comentarios

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -7,8 +7,7 @@ export const ItemList = ({products}) => {
 
         {products.map( product => <Item key={product.id} {...product}  />)}
     </div>
-        {/* Enviamos las props desestructuradas */}
-        {/* {products.map( product => <Item key={product.id} {...product}  />)} */}
+        
     </>
     )
 }
